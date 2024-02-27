f9448cc0e54f55b1cd0b03eae5af0b521659ad47
Sad Gabi <sadgabi20@gmail.com>
2024-02-26 21:31:10 -0600

    Minor changes
    Fixed typeWriter function and put header inside a container

--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -8,6 +8,9 @@ async function typeWriter(elementToAnimate, speed, text, i = 0) {
     return new Promise((resolve) => {
         if (i < text.length) {
             elementToAnimate.innerHTML += text.charAt(i);
+            if (text.charAt(i) === '\n') {
+                elementToAnimate.innerHTML += '<br>';
+            }
             setTimeout(async () => {
                 await typeWriter(elementToAnimate, speed, text, i + 1);
                 resolve();
@@ -20,6 +23,6 @@ async function typeWriter(elementToAnimate, speed, text, i = 0) {
 
 setTimeout(() => {
     (async () => {
-        await typeWriter(textHeader, 30, `fight\nthe\ncrime_`);
+        await typeWriter(textHeader, 50, `fight the\ncrime_`);
     })();
 }, 3500);
